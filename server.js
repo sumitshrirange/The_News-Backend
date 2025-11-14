68e68d2494c42f87455a624e04832af5992a01db
Sumit Shrirange <sumitshrirange81@gmail.com>
2025-11-14 23:24:16 +0530

    small error fixed commit

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-// const dotenv = require("dotenv")
+const dotenv = require("dotenv")
 const connectDB = require("./config/db.js");
 
 const authRoutes = require("./routes/auth.routes");
@@ -8,14 +8,17 @@ const userRoutes = require("./routes/user.routes");
 const newsRoutes = require("./routes/news.routes.js");
 const summarizeRoutes = require("./routes/summarize.routes.js");
 
-// dotenv.config();
+dotenv.config();
 connectDB();
 
 const app = express();
 // const PORT = process.env.PORT;
 
 // Middleware
-app.use(cors()); // Allow frontend requests from any origin (or restrict to the frontend URL)
+app.use(cors({
+  origin: ["http://localhost:5173", `${process.env.FRONTEND_URL}`],
+  credentials: true,
+})); // Allow frontend requests from any origin (or restrict to the frontend URL)
 app.use(express.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
