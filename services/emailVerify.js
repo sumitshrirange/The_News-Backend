f7330535f6dc41b8b220c5f13b08ad22746ac5f6
Sumit Shrirange <sumitshrirange81@gmail.com>
2025-11-15 01:39:27 +0530

    small changes commit

--- services/emailVerify.js
+++ services/emailVerify.js
@@ -1,4 +1,5 @@
 const { Resend } = require("resend");
+const dotenv = require("dotenv").config()
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 const verifyEmail = async (token, email) => {
