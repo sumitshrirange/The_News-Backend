b191d73b5b03efbd0e244ec4bdca9b9115739f37
Sumit Shrirange <sumitshrirange81@gmail.com>
2025-11-15 21:42:08 +0530

    debugging commit

--- server.js
+++ server.js
@@ -20,6 +20,10 @@ app.use(express.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  res.send("The News Backend is Running...");
+});
+
 // Routes
 app.use("/api/news", newsRoutes);
 app.use("/api/summarize", summarizeRoutes);
